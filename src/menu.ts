ec5ac0171645ac5cb2af76afd040eff3b37f4c95
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-01-03 10:03:40 +0100

    Let "getSubMenuByKey" take levels as param

--- src/menu.ts
+++ src/menu.ts
@@ -4,10 +4,10 @@ import {get as getContent} from "enonic-fp/lib/content";
 import {getSubMenus} from "enonic-fp/lib/menu";
 import {EnonicError} from "enonic-fp/lib/errors";
 
-export function getSubMenuByKey(key?: string): IOEither<EnonicError, ReadonlyArray<MenuItem>> {
+export function getSubMenuByKey(levels: number, key?: string): IOEither<EnonicError, ReadonlyArray<MenuItem>> {
   return key
     ? chain(
-      getSubMenus(2)
+      getSubMenus(levels)
     )(getContent({ key }))
     : right([]);
 }
