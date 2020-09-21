e89d926226a1f026bdd65bd2ca62c21f87252d62
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-09-21 14:37:59 +0200

    Add Context.chainRun
    Closes: ItemConsulting/enonic-fp#34

--- src/context.ts
+++ src/context.ts
@@ -1,4 +1,12 @@
 import {run} from "enonic-fp/context";
+import {chain, IOEither} from "fp-ts/IOEither";
+import {RunContext} from "enonic-types/context";
+
+export function chainRun(runContext: RunContext)
+  : <E, A, B>(f: (a: A) => IOEither<E, B>) => (ma: IOEither<E, A>) => IOEither<E, B> {
+
+  return <E, A, B>(f: (a: A) => IOEither<E, B>) => chain((a: A) => run(runContext)(f(a)))
+}
 
 export const runAsSuperUser = run({
   user: {
@@ -8,5 +16,16 @@ export const runAsSuperUser = run({
 });
 
 export const runInDraftContext = run({
+  branch: 'draft'
+});
+
+export const chainRunAsSuperUser = chainRun({
+  user: {
+    login: "su",
+    idProvider: 'system'
+  }
+});
+
+export const chainRunInDraftContext = chainRun({
   branch: 'draft'
 });
