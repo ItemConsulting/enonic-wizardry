fb24e329bc10804bf9dabf93e94a639087a9e28c
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-11-03 08:58:43 +0100

    Add i18n to validation + 2 validators
    This commit lets the user specify a "i18nPrefix", that can be used
    to look up i18n-keys, so that each field can have its own customized
    message.
    
    It also adds two validators that can be used by "xp-codegen-plugin":
     * RegexpValidatedString
     * MaxLengthValidatedString

--- src/reporters/ErrorDetailReporter.ts
+++ src/reporters/ErrorDetailReporter.ts
@@ -2,19 +2,72 @@ import {Context, ValidationError} from 'io-ts';
 import {Reporter} from "io-ts/lib/Reporter";
 import {fold} from 'fp-ts/Either';
 import {ErrorDetail} from "enonic-fp/errors";
+import {localizeFirst} from "enonic-fp/i18n";
+import {pipe} from "fp-ts/pipeable";
+import {getOrElse} from "fp-ts/Option";
 
-export const ErrorDetailReporter: Reporter<Array<ErrorDetail>> = {
-  report: fold(
-    (es) => es.map(validationErrorToErrorDetail),
-    () => []
-  )
-};
-
-function validationErrorToErrorDetail(err: ValidationError): ErrorDetail {
+export function getErrorDetailReporter(i18nKey?: string): Reporter<Array<ErrorDetail>> {
   return {
-    key: getPathInInterface(err.context),
-    message: err.message ?? 'Invalid value'
-  };
+    report: fold(
+      (es) => es.map(err => validationErrorToErrorDetail(err, i18nKey)),
+      () => []
+    )
+  }
+}
+
+function validationErrorToErrorDetail(err: ValidationError, i18nPrefix = "errors"): ErrorDetail {
+  const key = getPathInInterface(err.context);
+
+  return pipe(
+    getMessageKeys(key, isLastEmpty(err.context), i18nPrefix),
+    localizeFirst,
+    getOrElse(() => err.message ?? 'Invalid value'),
+    (message) => (
+      {
+        key,
+        message
+      }
+    )
+  );
+}
+
+function getMessageKeys(key: string, fieldIsEmpty: boolean, i18nPrefix?: string) {
+  const keyedMessageKeys = [
+    `${i18nPrefix}.bad-request-error.${key}`,
+    `${i18nPrefix}.400.${key}`,
+    `${i18nPrefix}.${key}`
+  ];
+
+  const emptyMessageKeys =  [
+    `errors.bad-request-error.defaultEmpty`,
+    `errors.400.defaultEmpty`
+  ];
+
+  const defaultMessageKeys = [
+    `${i18nPrefix}.bad-request-error`,
+    `${i18nPrefix}.400`,
+    `errors.bad-request-error.default`,
+    `errors.400.default`
+  ];
+
+  return keyedMessageKeys
+    .concat(fieldIsEmpty ? emptyMessageKeys : [])
+    .concat(defaultMessageKeys);
+}
+
+function isLastEmpty(context: Context): boolean {
+  const entries = context.filter(c => c.key !== "");
+  const last = entries[entries.length - 1];
+
+  return (last.actual === undefined)
+    || (last.actual === null)
+    || isString(last.actual)
+      ? last.actual?.length === 0
+      : false
+}
+
+function isString(s: unknown): s is string {
+  return typeof s === "string";
 }
 
 function getPathInInterface(context: Context): string {
--- src/validation.ts
+++ src/validation.ts
@@ -1,11 +1,12 @@
 import {Errors, Type} from 'io-ts';
-import {Either} from 'fp-ts/Either'
-import {ErrorDetailReporter} from "./reporters/ErrorDetailReporter";
+import * as t from 'io-ts';
+import {Either, either} from 'fp-ts/Either'
+import {getErrorDetailReporter} from "./reporters/ErrorDetailReporter";
 import {badRequestError, EnonicError} from 'enonic-fp/errors';
 import {fromEither, IOEither, mapLeft} from "fp-ts/IOEither";
 import {pipe} from "fp-ts/pipeable";
 
-export function validate<A, O = A, I = unknown>(a: Type<A, O, I>): (i: I) => IOEither<EnonicError, A> {
+export function validate<A, O = A, I = unknown>(a: Type<A, O, I>, i18nPrefix?: string): (i: I) => IOEither<EnonicError, A> {
   return (i: I): IOEither<EnonicError, A> => {
     const decoded: Either<Errors, A> = a.decode(i);
 
@@ -15,9 +16,29 @@ export function validate<A, O = A, I = unknown>(a: Type<A, O, I>): (i: I) => IOE
       mapLeft(() => (
         {
           ...badRequestError,
-          errors: ErrorDetailReporter.report(decoded)
+          errors: getErrorDetailReporter(i18nPrefix).report(decoded)
         }
       ))
     )
   };
 }
+
+export const RegexpValidatedString = (regexp: RegExp): t.Type<string, string, unknown> => new t.Type<string, string, unknown>(
+  'RegexpValidatedString',
+  (u): u is string => t.string.is(u) && regexp.test(String(u)),
+  (u, c) =>
+    either.chain(t.string.validate(u, c), s => {
+      return regexp.test(s) ? t.success(s) : t.failure(u, c)
+    }),
+  t.identity
+);
+
+export const MaxLengthValidatedString = (maxLength: number): t.Type<string, string, unknown> => new t.Type<string, string, unknown>(
+  'MaxLengthValidatedString',
+  (u): u is string => t.string.is(u) && u.length <= maxLength,
+  (u, c) =>
+    either.chain(t.string.validate(u, c), s => {
+      return s.length <= maxLength ? t.success(s) : t.failure(u, c)
+    }),
+  t.identity
+);
