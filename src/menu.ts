7d38153000e4f1cef79ed9dff30daacb33b089e8
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-12-28 11:54:32 +0100

    Update dependencies

--- src/menu.ts
+++ src/menu.ts
@@ -1,15 +1,15 @@
 import {chain, IOEither, right} from "fp-ts/IOEither";
-import {MenuItem} from "enonic-types/menu";
+import {GetMenuParams, MenuItem} from "enonic-types/menu";
 import {get as getContent} from "enonic-fp/content";
 import {getSubMenus} from "enonic-fp/menu";
 import {EnonicError} from "enonic-fp/errors";
 import {pipe} from "fp-ts/pipeable";
 
-export function getSubMenuByKey(levels: number, key?: string): IOEither<EnonicError, ReadonlyArray<MenuItem>> {
+export function getSubMenuByKey(levels: number, params?: GetMenuParams, key?: string): IOEither<EnonicError, ReadonlyArray<MenuItem>> {
   return (key !== undefined)
     ? pipe(
         getContent({ key }),
-        chain(getSubMenus(levels))
+        chain(getSubMenus(levels, params))
       )
     : right([]);
 }
